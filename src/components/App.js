481ede3f0bf095cb2f4b0ea9d9846e2172cd4b90
Labeebopc <Labeebopc313@gmail.com>
2023-01-13 18:05:18 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component, useState} from "react";
+import '../styles/App.css';
+import SignUp from "./sign-up/SignUp";
+
+const App = () => {
+  return (
+    <div id="main">
+      <SignUp/>
+    </div>
+  )
+}
+
+
+export default App;
